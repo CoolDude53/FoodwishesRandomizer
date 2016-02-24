ce8943baa41537aad4fddc36ca1afb08139e5d30
CoolDude53 <pickle.plaza@gmail.com>
2016-02-23 23:08:35 -0500

    Added site indexing to maximize efficiency.

---
+++ out/production/FoodwishesRandomizer/README.md
@@ -0,0 +1,6 @@
+# FoodwishesRandomizer
+A simple program that takes every recipe/and occasional random blog post from http://foodwishes.blogspot.com/ and randomly selects one.
+
+There are still issues with efficiency, but that can only be expected when crawling hundreds of urls.
+
+Jsoup is a dependency, FoodwishesRandomizer.jar is the jar output of the latest build of the program.
--- src/Randomizer.java
+++ src/Randomizer.java
@@ -1,11 +1,18 @@
 package src;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 import org.jsoup.Jsoup;
 
 import javax.swing.*;
 import javax.swing.event.HyperlinkEvent;
 import java.awt.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.SecureRandom;
@@ -15,90 +22,85 @@ public class Randomizer
 {
     public static void main(String args[]) throws IOException
     {
-        JOptionPane.showConfirmDialog(null, "Click to find a random recipe from Chef John!", "FoodwishesRandomizer", JOptionPane.DEFAULT_OPTION);
-
-        HTMLScanner scanner;
-        String url = "http://foodwishes.blogspot.com/";
-        int year = 2007;
-        int month = 1;
-        ConcurrentHashMap<String, String> recipes = new ConcurrentHashMap<>();
-
-        while (year < 2020)
+        File file = new File(System.getProperty("user.home") + "/index.json");
+        ConcurrentHashMap<String, String> recipes;
+        Type mapType = new TypeToken<ConcurrentHashMap<String, String>>()
         {
-            while (month < 12)
+        }.getType();
+
+        if (JOptionPane.showOptionDialog(null, "Click a button to find a random recipe from Chef John!", "FoodwishesRandomizer", JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, new String[]{"Update website index.", "Find a random recipe."}, null) == 1)
+        {
+            if (file.exists())
             {
-                String monthURL = "/" + (1 < 10 ? "0" + month : month) + "/";
+                JsonReader jsonReader = new JsonReader(new FileReader(file));
+                recipes = new Gson().fromJson(jsonReader, mapType);
+            }
+            else
+            {
+                recipes = new ConcurrentHashMap<>();
+                HTMLScanner htmlScanner;
+                String url = "http://foodwishes.blogspot.com/";
+                int year = 2007;
+                int month = 1;
 
-                try
+                while (year < 2020)
                 {
-                    scanner = new HTMLScanner(url + year + monthURL);
-                } catch (IOException e)
-                {
-                    break;
-                }
-
-                while (scanner.hasNextLink())
-                {
-                    String nextLink = scanner.nextLink();
-
-                    if (nextLink.startsWith(url + year + monthURL) && !recipes.containsKey(nextLink))
+                    while (month < 12)
                     {
-                            new Thread(() -> {
-                                try
-                                {
-                                    recipes.put(nextLink, Jsoup.connect(nextLink).get().getElementsByTag("h3").text());
-                                } catch (Exception e)
-                                {
-                                }
-                            }).run();
+                        String monthURL = "/" + (month < 10 ? "0" + month : month) + "/";
+
+                        try
+                        {
+                            htmlScanner = new HTMLScanner(url + year + monthURL);
+                        } catch (IOException e)
+                        {
+                            break;
+                        }
+
+                        while (htmlScanner.hasNextLink())
+                        {
+                            String nextLink = htmlScanner.nextLink();
+
+                            if (nextLink.startsWith(url + year + monthURL) && !recipes.containsKey(nextLink))
+                            {
+                                new Thread(() -> {
+                                    try
+                                    {
+                                        recipes.put(nextLink, Jsoup.connect(nextLink).get().getElementsByTag("h3").text());
+                                    } catch (Exception e)
+                                    {
+                                        e.printStackTrace();
+                                    }
+                                }).run();
+                            }
+                        }
+
+                        month++;
                     }
+
+                    year++;
+                    month = 1;
                 }
 
-                month++;
+                String json = new Gson().toJson(recipes);
+                FileWriter fileWriter = new FileWriter(file);
+                fileWriter.write(json);
+                fileWriter.flush();
+                fileWriter.close();
             }
 
-            year++;
-            month = 1;
-        }
+            String recipeURL = (String) recipes.keySet().toArray()[new SecureRandom().nextInt(recipes.size())];
 
-        String recipeURL = (String) recipes.keySet().toArray()[new SecureRandom().nextInt(recipes.size())];
+            JEditorPane editorPane = new JEditorPane("text/html", "<html><center><a href=\"" + recipeURL + "\">" + recipes.get(recipeURL) + "</a></center></html>");
 
-        JEditorPane editorPane = new JEditorPane("text/html", "<html><center><a href=\"" + recipeURL + "\">" + recipes.get(recipeURL) + "</a></center></html>");
+            Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
 
-        Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
-
-        editorPane.addHyperlinkListener(e -> {
-            if (e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED) && desktop != null && desktop.isSupported(Desktop.Action.BROWSE))
-            {
-                try
-                {
-                    desktop.browse(new URI(recipeURL));
-                } catch (IOException | URISyntaxException e1)
-                {
-                    e1.printStackTrace();
-                }
-            }
-        });
-
-        editorPane.setEditable(false);
-        editorPane.setBackground(new JLabel().getBackground());
-
-        int reRandomize = JOptionPane.showOptionDialog(null, editorPane, "Recipe", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, (new String[]{"Another Recipe?"}), 0);
-
-        while (reRandomize == 0)
-        {
-            String newRecipeURL = (String) recipes.keySet().toArray()[new SecureRandom().nextInt(recipes.size())];
-
-            JEditorPane newEditorPane = new JEditorPane("text/html", "<html><center><a href=\"" + newRecipeURL + "\">" + recipes.get(newRecipeURL) + "</a></center></html>");
-
-            Desktop newDesktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
-
-            newEditorPane.addHyperlinkListener(e -> {
-                if (e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED) && newDesktop != null && newDesktop.isSupported(Desktop.Action.BROWSE))
+            editorPane.addHyperlinkListener(e -> {
+                if (e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED) && desktop != null && desktop.isSupported(Desktop.Action.BROWSE))
                 {
                     try
                     {
-                        newDesktop.browse(new URI(newRecipeURL));
+                        desktop.browse(new URI(recipeURL));
                     } catch (IOException | URISyntaxException e1)
                     {
                         e1.printStackTrace();
@@ -106,10 +108,90 @@ public class Randomizer
                 }
             });
 
-            newEditorPane.setEditable(false);
-            newEditorPane.setBackground(new JLabel().getBackground());
+            editorPane.setEditable(false);
+            editorPane.setBackground(new JLabel().getBackground());
 
-            reRandomize = JOptionPane.showOptionDialog(null, newEditorPane, "Recipe", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, (new String[]{"Find another recipe?"}), 0);
+            int reRandomize = JOptionPane.showOptionDialog(null, editorPane, "Recipe", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, new String[]{"Another Recipe?"}, null);
+
+            while (reRandomize == 0)
+            {
+                String newRecipeURL = (String) recipes.keySet().toArray()[new SecureRandom().nextInt(recipes.size())];
+
+                JEditorPane newEditorPane = new JEditorPane("text/html", "<html><center><a href=\"" + newRecipeURL + "\">" + recipes.get(newRecipeURL) + "</a></center></html>");
+
+                Desktop newDesktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+
+                newEditorPane.addHyperlinkListener(e -> {
+                    if (e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED) && newDesktop != null && newDesktop.isSupported(Desktop.Action.BROWSE))
+                    {
+                        try
+                        {
+                            newDesktop.browse(new URI(newRecipeURL));
+                        } catch (IOException | URISyntaxException e1)
+                        {
+                            e1.printStackTrace();
+                        }
+                    }
+                });
+
+                newEditorPane.setEditable(false);
+                newEditorPane.setBackground(new JLabel().getBackground());
+
+                reRandomize = JOptionPane.showOptionDialog(null, newEditorPane, "Recipe", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, new String[]{"Find another recipe?"}, null);
+            }
+        }
+        else
+        {
+            recipes = new ConcurrentHashMap<>();
+            HTMLScanner htmlScanner;
+            String url = "http://foodwishes.blogspot.com/";
+            int year = 2007;
+            int month = 1;
+
+            while (year < 2020)
+            {
+                while (month < 12)
+                {
+                    String monthURL = "/" + (month < 10 ? "0" + month : month) + "/";
+
+                    try
+                    {
+                        htmlScanner = new HTMLScanner(url + year + monthURL);
+                    } catch (IOException e)
+                    {
+                        break;
+                    }
+
+                    while (htmlScanner.hasNextLink())
+                    {
+                        String nextLink = htmlScanner.nextLink();
+
+                        if (nextLink.startsWith(url + year + monthURL) && !recipes.containsKey(nextLink))
+                        {
+                            new Thread(() -> {
+                                try
+                                {
+                                    recipes.put(nextLink, Jsoup.connect(nextLink).get().getElementsByTag("h3").text());
+                                } catch (Exception e)
+                                {
+                                    e.printStackTrace();
+                                }
+                            }).run();
+                        }
+                    }
+
+                    month++;
+                }
+
+                year++;
+                month = 1;
+            }
+
+            String json = new Gson().toJson(recipes);
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(json);
+            fileWriter.flush();
+            fileWriter.close();
         }
     }
 }
